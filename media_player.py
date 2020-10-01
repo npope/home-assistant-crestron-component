31cbcc689eda5d1473a1bc5013f68c610fa8ecd1
Nick Pope <nick.pope@gmail.com>
2020-10-01 13:59:51 +0000

    Fix source/source_select logic in media_player.py

--- media_player.py
+++ media_player.py
@@ -30,8 +30,8 @@ class CrestronRoom(MediaPlayerEntity):
         self._mute_join = config['mute_join']
         self._volume_join = config['volume_join']
         self._source_number_join = config['source_number_join']
-        self._source_list = config['source_list']
-        _LOGGER.debug(f'media player source list {self._source_list.values()}')
+        self._sources = dict(config['sources'])
+        _LOGGER.debug(f'media player source list {self._sources}')
 
     async def async_added_to_hass(self):
         self._hub.register_callback(self.async_write_ha_state)
@@ -61,7 +61,7 @@ class CrestronRoom(MediaPlayerEntity):
 
     @property
     def source_list(self):
-        self._source_list.values()
+        return list(self._sources.values())
 
     @property
     def source(self):
@@ -69,7 +69,7 @@ class CrestronRoom(MediaPlayerEntity):
          if source_num == 0:
              return None
          else:
-             return self._source_list[source_num]
+             return self._sources[source_num]
 
     @property
     def state(self):
@@ -87,15 +87,16 @@ class CrestronRoom(MediaPlayerEntity):
          return self._hub.get_analog(self._volume_join)/65535
 
     async def async_mute_volume(self, mute):
+        _LOGGER.info(f'got mute {mute}')
         self._hub.set_digital(self._mute_join, mute)
 
     async def async_set_volume_level(self, volume):
         self._hub.set_analog(self._volume_join, int(volume*65535))
 
     async def async_select_source(self, source):
-        for k,v in self._source_list.items():
-            if v == source:
-                self._hub.set_analog(self._source_number_join, k)
+        for input_num, name in self._sources.items():
+            if name == source:
+                self._hub.set_analog(self._source_number_join, input_num)
 
     async def async_turn_off(self):
         self._hub.set_analog(self._source_number_join, 0)
