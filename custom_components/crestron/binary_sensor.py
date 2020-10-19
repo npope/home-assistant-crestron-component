43f92ec18621976ea9d01117c391a412cab2b3d7
Nick Pope <nick.pope@gmail.com>
2020-10-19 11:54:49 -0400

    Add PLATFORM_SCHEMA to binary_sensor.py

--- custom_components/crestron/binary_sensor.py
+++ custom_components/crestron/binary_sensor.py
@@ -1,13 +1,23 @@
 """Platform for Crestron Binary Sensor integration."""
 
+import voluptuous as vol
+import logging
+
 from homeassistant.helpers.entity import Entity
 from homeassistant.const import STATE_ON, STATE_OFF, CONF_NAME, CONF_DEVICE_CLASS
-from .const import HUB, DOMAIN, CONF_JOIN, CONF_IS_ON_JOIN
+import homeassistant.helpers.config_validation as cv
 
-import logging
+from .const import HUB, DOMAIN, CONF_JOIN, CONF_IS_ON_JOIN
 
 _LOGGER = logging.getLogger(__name__)
 
+PLATFORM_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_NAME): cv.string,
+        vol.Required(CONF_IS_ON_JOIN): cv.positive_integer,           
+        vol.Required(CONF_DEVICE_CLASS): cv.string,
+    }
+)
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     hub = hass.data[DOMAIN][HUB]
@@ -18,9 +28,9 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 class CrestronBinarySensor(Entity):
     def __init__(self, hub, config):
         self._hub = hub
-        self._name = config[CONF_NAME]
-        self._join = config[CONF_IS_ON_JOIN]
-        self._device_class = config[CONF_DEVICE_CLASS]
+        self._name = config.get(CONF_NAME)
+        self._join = config.get(CONF_IS_ON_JOIN)
+        self._device_class = config.get(CONF_DEVICE_CLASS)
 
     async def async_added_to_hass(self):
         self._hub.register_callback(self.process_callback)
