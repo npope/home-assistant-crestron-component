2196df14d56af33b7f5f2e53f56e28cacfe70cdc
Nick Pope <nick.pope@gmail.com>
2020-10-19 13:04:32 -0400

    add PLATFORM_SCHEMA to switch.py

--- custom_components/crestron/switch.py
+++ custom_components/crestron/switch.py
@@ -1,12 +1,22 @@
 """Platform for Crestron Switch integration."""
 
+import voluptuous as vol
+import logging
+
+import homeassistant.helpers.config_validation as cv
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.const import STATE_ON, STATE_OFF, CONF_NAME, CONF_DEVICE_CLASS
 from .const import HUB, DOMAIN, CONF_SWITCH_JOIN
-import logging
 
 _LOGGER = logging.getLogger(__name__)
 
+PLATFORM_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_NAME): cv.string,
+        vol.Optional(CONF_DEVICE_CLASS): cv.string,
+        vol.Required(CONF_SWITCH_JOIN): cv.positive_int,           
+    }
+)
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     hub = hass.data[DOMAIN][HUB]
@@ -17,12 +27,9 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 class CrestronSwitch(SwitchEntity):
     def __init__(self, hub, config):
         self._hub = hub
-        self._name = config[CONF_NAME]
-        self._switch_join = config[CONF_SWITCH_JOIN]
-        if CONF_DEVICE_CLASS in config:
-            self._device_class = config[CONF_DEVICE_CLASS]
-        else:
-            self._device_class = "switch"
+        self._name = config.get(CONF_NAME)
+        self._switch_join = config.get(CONF_SWITCH_JOIN)
+        self._device_class = config.get(CONF_DEVICE_CLASS, "switch")
 
     async def async_added_to_hass(self):
         self._hub.register_callback(self.process_callback)
